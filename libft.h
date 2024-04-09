abc679ef80d5fc96fd1b35f63be528c2c04b3390
BenjaminHThomas <benjamin.h.thomas1@gmail.com>
2024-04-09 12:27:03 +0200

    added ft_lstlast

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bento </var/spool/mail/bento>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 12:04:30 by bento             #+#    #+#             */
-/*   Updated: 2024/04/09 12:17:56 by bthomas          ###   ########.fr       */
+/*   Updated: 2024/04/09 12:26:46 by bthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,6 @@ void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
