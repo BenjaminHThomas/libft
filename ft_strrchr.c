a3718090d6e0bbea78d8383fe7834464b09f2213
BenjaminHThomas <benjamin.h.thomas1@gmail.com>
2024-04-10 18:51:03 +0200

    fixed bug with 2 null entries

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,23 +6,29 @@
 /*   By: bthomas <bthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 16:09:14 by bthomas           #+#    #+#             */
-/*   Updated: 2024/03/30 16:38:54 by bthomas          ###   ########.fr       */
+/*   Updated: 2024/04/10 18:47:23 by bthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strrchr(const char *s, int c)
 {
-	char	*ps;
+	char			*ps;
+	unsigned char	c2;
 
+	if (!s)
+		return (0);
+	c2 = (unsigned char)c;
 	ps = 0;
 	while (*s)
 	{
-		if (*s == c)
+		if (*s == c2)
 			ps = (char *)s;
 		s++;
 	}
-	if (ps == 0)
-		return ((char *)s);
+	if (c == '\0' && *s == '\0')
+		return (char *)s;
 	return (ps);
 }
 /*
