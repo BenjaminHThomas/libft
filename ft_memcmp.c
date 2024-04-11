4e3d0f35846d8a71d4fe2b4789c2219f7792dddb
BenjaminHThomas <benjamin.h.thomas1@gmail.com>
2024-04-11 18:45:20 +0200

    fixed memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,11 +6,12 @@
 /*   By: bthomas <bthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 17:37:19 by bthomas           #+#    #+#             */
-/*   Updated: 2024/03/31 15:13:19 by bthomas          ###   ########.fr       */
+/*   Updated: 2024/04/11 18:44:53 by bthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+/*#include <stddef.h>*/
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
@@ -21,12 +22,14 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 		return (0);
 	ps1 = (unsigned char *)s1;
 	ps2 = (unsigned char *)s2;
-	while (*ps1 != *ps2 && n && *ps1 && *ps2)
+	while (n && *ps1 == *ps2)
 	{
 		ps1++;
 		ps2++;
 		n--;
 	}
+	if (n == 0)
+		return (0);
 	return (*ps1 - *ps2);
 }
 /*
@@ -35,11 +38,14 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 int	main(void)
 {
-	char str1[15] = "abcdef";
-	char str2[15] = "ABCDEF";
+	char str1[15] = "123456789";
+	char str2[15] = "123459876";
 
-	int ret = ft_memcmp(str1, str2, 5);
-	printf("%d", ret);
+	int ret;
+	ret = ft_memcmp(str1, str2, 5);
+	printf("mine: %d\n", ret);
+	ret = memcmp(str1, str2, 5);
+	printf("real: %d\n", ret);
 	return (0);
 }
 */
