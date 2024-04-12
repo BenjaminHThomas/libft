9dc72bb52664fb5a917434dd0548e82616270cdc
BenjaminHThomas <benjamin.h.thomas1@gmail.com>
2024-04-12 12:35:07 +0200

    minor edits

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: bthomas <bthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 13:50:07 by bthomas           #+#    #+#             */
-/*   Updated: 2024/03/28 16:09:41 by bthomas          ###   ########.fr       */
+/*   Updated: 2024/04/11 11:14:41 by bthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@ int	ft_strlen(const char *s)
 {
 	int	len;
 
-	if (!s)
-		return (0);
 	len = 0;
 	while (s[len])
 		len++;
