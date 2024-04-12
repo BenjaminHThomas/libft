149699fa0892e24ef67987f1fb02b21f83131691
BenjaminHThomas <benjamin.h.thomas1@gmail.com>
2024-04-12 10:11:10 +0200

    fixed strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,25 +6,26 @@
 /*   By: bthomas <bthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 09:29:03 by bthomas           #+#    #+#             */
-/*   Updated: 2024/03/31 10:09:25 by bthomas          ###   ########.fr       */
+/*   Updated: 2024/04/12 10:10:55 by bthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+/*#include <string.h>
+#include <stddef.h>*/
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	const char	*plit;
 	const char	*pbig;
 
+	if (little[0] == 0)
+		return ((char *)big);
 	while (*big && len)
 	{
 		plit = little;
-		while (*big != *little && len && *big)
-		{
-			big++;
+		while (*big != *little && len && *big++)
 			len--;
-		}
 		if (*big == *little && len)
 		{
 			pbig = big;
@@ -44,10 +45,11 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 #include <stdio.h>
 int	main(void)
 {
-	char	*big = "Find the WOW in this";
-	char	little[10] = "this";
-	big = ft_strnstr(big, little, 20);
-	printf("%s", big);
+	char	*big = "oh no not the empty string!";
+	char	*little = "";
+	char	*big_mine;
+	big_mine = ft_strnstr(big, little, 20);
+	printf("Mine: %s", big_mine);
 	return (0);
 }
 */
