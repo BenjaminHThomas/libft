5c0b55974152cc42885581f9c429e3ddea684f3c
BenjaminHThomas <benjamin.h.thomas1@gmail.com>
2024-05-14 19:02:21 +0200

    now works backwards

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,12 +6,13 @@
 /*   By: bthomas <bthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 16:09:14 by bthomas           #+#    #+#             */
-/*   Updated: 2024/04/12 16:59:12 by bthomas          ###   ########.fr       */
+/*   Updated: 2024/05/14 19:01:42 by bthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-//#include <stddef.h>
+/*#include <stddef.h>
+#include <string.h>*/
 
 char	*ft_strrchr(const char *s, int c)
 {
@@ -19,25 +20,25 @@ char	*ft_strrchr(const char *s, int c)
 	unsigned char	c2;
 
 	c2 = (unsigned char)c;
-	ps = NULL;
-	while (*s)
+	ps = (char *)s + ft_strlen(s);
+	if (c2 == '\0')
+		return (ps);
+	while (ps >= s)
 	{
-		if (*s == c2)
-			ps = (char *)s;
-		s++;
+		if (*ps == c2)
+			return (ps);
+		ps--;
 	}
-	if (c == '\0')
-		return ((char *)s);
+	ps = NULL;
 	return (ps);
 }
 /*
 #include <stdio.h>
-#include <string.h>
+
 int	main(void)
 {
-	char *str = ft_strrchr("teste", '\0');
-	char *str2 = strrchr("teste", '\0');
+	char *str = ft_strrchr("teste", 'a');
+	char *str2 = strrchr("teste", 'a');
 	printf("mine: %s\nreal: %s", str, str2);
 	return (0);
-}
-*/
+}*/
